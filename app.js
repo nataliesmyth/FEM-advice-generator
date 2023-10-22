c84b99e4b1ef54660ea2b619b20643c7a055f63a
Nataliesmyth <nataliesmyth11@gmail.com>
2023-10-22 17:27:27 -0400

    mobile mvp design complete

---
+++ app.js
@@ -0,0 +1,15 @@
+const quote = document.querySelector('#quote')
+const adviceId = document.querySelector('#adviceId');
+const reload = document.querySelector('#reload')
+async function FetchData(){
+    var response = await fetch('https://api.adviceslip.com/advice');
+    let json = await response.json();
+    console.log(json);
+    adviceId.innerText = `${json.slip.id}`;
+    console.log(adviceId)
+    quote.innerText = `"${json.slip.advice}"`;
+}
+
+FetchData();
+
+reload.addEventListener('click', FetchData);
