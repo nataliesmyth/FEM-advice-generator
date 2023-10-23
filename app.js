eee9c22e8063b6a8aed45112e91dbf0510a6b59e
Nataliesmyth <nataliesmyth11@gmail.com>
2023-10-23 15:26:37 -0400

    modified fetch request to generate advice faster in all browsers

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const quote = document.querySelector('#quote')
 const adviceId = document.querySelector('#adviceId');
 const reload = document.querySelector('#reload')
 async function FetchData(){
-    var response = await fetch('https://api.adviceslip.com/advice');
+    var response = await fetch("https://api.adviceslip.com/advice", {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache",
+      });
     let json = await response.json();
     console.log(json);
     adviceId.innerText = `${json.slip.id}`;
